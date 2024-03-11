487b2affbb60cab743be362f2cadc8ff681144b2
Azujito <virote3011@gmail.com>
2024-03-11 22:17:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ from flask import Flask, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient("mongodb+srv://datafromgooglesheet:kaluiklui3011@cluster0.7vgowbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client.weather
 
-@app.route("/", methods=["GET"])
-def dataforesp():
-    collection = db.data
-    lastest_data = collection.find_one({}, {"Temperature": 1,"Humidity": 1,"Status": 1, "HumidityADJ": 1, "_id": 0}, sort=[('_id', -1)])
-    return jsonify(lastest_data)
+# เชื่อมต่อ MongoDB
+client = MongoClient('mongodb+srv://virote3011:kaluiklui3011@cluster0.ivzlivk.mongodb.net/?retryWrites=true&w=majority')
+db = client.AEP  # เลือกฐานข้อมูลที่ต้องการใช้งาน
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# Route สำหรับดึงข้อมูล seconds_amount ล่าสุดจาก MongoDB และส่งออกเป็น JSON
+@app.route('/seconds_amount', methods=['GET'])
+def get_seconds_amount():
+    collection = db.scheduler  # เลือกคอลเล็กชันที่ต้องการใช้งาน
+    # ดึงข้อมูล seconds_amount ล่าสุด
+    latest_data = collection.find_one({}, {'_id': 0, 'seconds_amount': 1}, sort=[('_id', -1)])
+
+    # แปลงข้อมูลให้เป็นรูปแบบ JSON ด้วย jsonify() ฟังก์ชันของ Flask
+    return jsonify(latest_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
