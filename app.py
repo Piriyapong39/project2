fea5f5070d6e8c5b8c5b8a529a54cdb9ade62bb5
Azujito <virote3011@gmail.com>
2024-03-11 22:09:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
+
 app = Flask(__name__)
-client = MongoClient('mongodb+srv://virote3011:kaluiklui3011@cluster0.ivzlivk.mongodb.net/?retryWrites=true&w=majority')
-db = client.AEP
-@app.route('/', methods=['GET'])
-def get_seconds_amount():
-    collection = db.scheduler
-    latest_data = collection.find_one({}, {'_id': 0, 'seconds_amount': 1}, sort=[('_id', -1)])
-    return jsonify(latest_data)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+client = MongoClient("mongodb+srv://datafromgooglesheet:kaluiklui3011@cluster0.7vgowbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client.weather
+
+@app.route("/", methods=["GET"])
+def dataforesp():
+    collection = db.data
+    lastest_data = collection.find_one({}, {"Temperature": 1,"Humidity": 1,"Status": 1, "HumidityADJ": 1, "_id": 0}, sort=[('_id', -1)])
+    return jsonify(lastest_data)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
